15a3631bfcd5364fc5c013470e8120c268366fa0
tknzk <tkm.knzk@gmail.com>
2016-02-26 17:56:42 +0900

    fix condition

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,7 @@ var now = new Date();
 var article_date = Date.parse($(".teamArticle_header_date").html());
 var diff = now - article_date;
 var diff_day = diff / 86400000;
-if (diff_day > 365) {
+if (diff_day >= 365) {
   var div = $(".teamArticle_header_titleContainer");
   var message = '<div class="box oldest_article">この記事は更新から1年以上経過しています</div>'
   div.after(message)
