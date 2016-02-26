e199a0d4b0ce3c185d15534b4a24d7394411f9db
tknzk <tkm.knzk@gmail.com>
2016-02-26 17:55:34 +0900

    remove console.log

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,9 @@
-console.log($(".teamArticle_header_date").html());
 var now = new Date();
 var article_date = Date.parse($(".teamArticle_header_date").html());
 var diff = now - article_date;
 var diff_day = diff / 86400000;
-console.log(diff_day);
 if (diff_day > 365) {
   var div = $(".teamArticle_header_titleContainer");
-  var message = '<div class="box oldest_article">この記事は作成から1年以上経過しています</div>'
+  var message = '<div class="box oldest_article">この記事は更新から1年以上経過しています</div>'
   div.after(message)
 }
